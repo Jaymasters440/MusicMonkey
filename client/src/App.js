645982caa9031a479894a26e0ed06178fec71f8f
Nathan Stepper <nstepper@uw.edu>
2023-06-01 21:02:21 -0700

    added in Bulma

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Uncomment import statement below after building queries and mutations
- import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Home from './pages/Home';
 import Genre from './pages/Genre';
@@ -10,24 +8,32 @@ import Vote from './pages/Vote';
 
 function App() {
   return (
-    <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Routes>
-          <Route 
-            path="/" 
-            element={<Home />} 
-          />
-          <Route 
-            path="/genre" 
-            element={<Genre />} 
-          />
-          <Route 
-            path="/genre/:id" 
-            element={<Vote />} 
-          />
-        </Routes>
-      </div>
-    </Router>
+    <ApolloProvider client={new ApolloClient({ uri: 'your_graphql_endpoint', cache: new InMemoryCache() })}>
+      <Router>
+        <div className="black-background">
+          <div className="container is-centered">
+            <div className="columns is-centered">
+              <div className="column is-half">
+                <Routes>
+                  <Route 
+                    path="/" 
+                    element={<Home />} 
+                  />
+                  <Route 
+                    path="/genre" 
+                    element={<Genre />} 
+                  />
+                  <Route 
+                    path="/genre/:id" 
+                    element={<Vote />} 
+                  />
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
