f397c6809831b07055ede40816ad198a4e69cdfe
DKsolomon <DKSolomon8135@gmail.com>
2023-06-14 21:46:04 -0700

    changed uri link

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import AppNavbar from './conponents/navbar';
 import './index.css'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
